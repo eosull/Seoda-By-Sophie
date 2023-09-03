bdb9872a16206081f21c6d2efcb4ae5242087ff3
eosull <osullivaneoin442@gmail.com>
2023-09-03 16:04:15 +0000

    feat:customise product overview cards

--- products/views.py
+++ products/views.py
@@ -5,7 +5,7 @@ from .models import Product
 def all_products(request):
     # View returning all products, search and sort queries
 
-    products = Product.objects.all()
+    products = Product.objects.all().order_by('-is_new', 'is_featured')
 
     context = {
         'products': products
