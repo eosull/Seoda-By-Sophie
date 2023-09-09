4f43209be6b8cdd3377e44ad5c53224084bd8c2a
eosull <osullivaneoin442@gmail.com>
2023-09-09 13:01:11 +0000

    feat:add update and delete basket functionality

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -6,7 +6,7 @@ def view_basket(request):
     return render(request, 'basket/basket.html')
 
 
-def add_to_bag(request, product_id):
+def add_to_basket(request, product_id):
     # Add specified quantity of product to basket
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -19,3 +19,30 @@ def add_to_bag(request, product_id):
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, product_id):
+    # Adjust quantity of product in basket
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+    print(quantity)
+
+    if quantity > 0:
+        basket[product_id] = quantity
+    else:
+        basket.pop(product_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, product_id):
+    # Remove product from basket
+    try:
+        basket = request.session.get('basket', {})
+        basket.pop(product_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    except Exception as e:
+        return HttpResponse(status=500)
