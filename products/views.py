39c8389704b2e6242bc01023e908ffd28b290a6f
eosull <osullivaneoin442@gmail.com>
2023-09-12 19:25:34 +0000

    feat: add delete product functionality and admin defence

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
@@ -70,14 +71,19 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     # Add a product to the store
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only the site admin can do that')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             # messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         # else:
         #     messages.error(request, 'Failed to add product. Please ensure the form is valid.')
     else:
@@ -91,8 +97,13 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     # Edit a product
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only the site admin can do that')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -112,3 +123,32 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def confirm_delete(request, product_id):
+    # Confirm delete
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only the site admin can do that')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    template = 'products/delete_product.html'
+    context = {
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def delete_product(request, product_id):
+    # Delete a product
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only the site admin can do that')
+        return redirect(reverse('home'))
+        
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    # messages.success(request, 'Product deleted')
+    return redirect(reverse('products'))
