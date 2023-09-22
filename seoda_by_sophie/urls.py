f2e7608ab9fce1772b3ec2f859d3ee4a685c1397
eosull <osullivaneoin442@gmail.com>
2023-09-22 06:27:32 +0000

    style:customise about template

--- seoda_by_sophie/urls.py
+++ seoda_by_sophie/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-    path('about/', include('profiles.urls')),
+    path('about/', include('about.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
