71ee621274fddaec247ffb66da3c00e5c84f43ab
eosull <osullivaneoin442@gmail.com>
2023-09-23 08:19:37 +0000

    build:customise about admin panel

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import InfoCategory, Faq, Testimonial
 
-# Register your models here.
+
+class FaqAdmin(admin.ModelAdmin):
+    list_display = (
+        'question',
+        'created',
+        'updated',
+    )
+
+    ordering = ('-updated',)
+
+
+class InfoCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = (
+        'customer',
+        'product',
+    )
+
+
+admin.site.register(Faq, FaqAdmin)
+admin.site.register(InfoCategory, InfoCategoryAdmin)
+admin.site.register(Testimonial, TestimonialAdmin)
