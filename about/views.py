aba63580273767effe812e686446820d96c1b46e
eosull <osullivaneoin442@gmail.com>
2023-09-25 16:29:55 +0000

    fix:add render to string import

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from django.conf import settings
 
 from .forms import ContactForm
