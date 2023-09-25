53a8afc05e356f1bbbd89571b00744eced06f0ba
eosull <osullivaneoin442@gmail.com>
2023-09-25 06:37:27 +0000

    feat:add contact form template and links

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 from django.contrib import messages
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .forms import ContactForm
 from .models import Faq, InfoCategory
@@ -24,19 +26,36 @@ def faqs(request):
 
 def contact(request):
     # View to handle contact form submission
-    form_data = {
-        'name': request.POST['name'],
-        'email': request.POST['email'],
-        'subject': request.POST['subject'],
-        'message': request.POST['message'],
-    }
     if request.method == 'POST':
+        form_data = {
+            'name': request.POST['name'],
+            'email': request.POST['email'],
+            'subject': request.POST['subject'],
+            'message': request.POST['message'],
+        }
         contact_form = ContactForm(form_data)
-    if contact_form.is_valid():
-        # SEND EMAIL
-        messages.success(request, 'Message sent successfully! We will get back to you as soon as we can')
-        return redirect(reverse('home'))
+        if contact_form.is_valid():
+            # subject = f'New Message {form_data['subject']} from {form_data['name']}'
+            # body = f'{form_data['message']} From {form_data['name']} - {form_data['email']}'
+            # subject = 'Testing Email Subject'
+            # body = 'Testing Message Sending'
+        
+            # send_mail(
+            #     subject,
+            #     body,
+            #     settings.DEFAULT_FROM_EMAIL,
+            #     [settings.DEFAULT_FROM_EMAIL]
+            # ) 
+            messages.success(request, 'Message sent successfully! We will get back to you as soon as we can')
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, "Please check your form information")
     else:
-        messages.error(request, "Please check your form information")
+        contact_form = ContactForm()
     
-    return render(request, 'about/contact.html')
+    template = 'about/contact.html'
+    context = {
+        'contact_form': contact_form,
+    }
+
+    return render(request, template, context)
